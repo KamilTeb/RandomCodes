ea76165dfa9ac6a49f828d1e002b1838fe7dadd7
Kamil Trybel <ktrybel@teb-gdansk.ad>
2024-01-28 10:13:46 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const chars = 'ABCDEFGHIJK0123456789';
+
+// Pobieramy diva i button
+
+// Dwie zmienne globalne zawierające - ilość kodów, długość kodu
+
+// Funkcja losująca kody - tu trzeba użyć zagnieżdżenie pętli for
+
+// Nasłuchiwanie na button na powyższą funkcję 
